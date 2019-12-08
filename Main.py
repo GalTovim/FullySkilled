2bf0b176b381ff6d8b5c324f5a239477480154a8
Gal Tovim <gal2077@gmail.com>
2019-12-08 13:12:58 +0200

    Updated 1:12 PM

--- Main.py
+++ Main.py
@@ -2,21 +2,20 @@ import sqlite3
 from employee import Employee
 
 conn = sqlite3.connect('FullySkilled.db')
-
 c = conn.cursor()
 
 #c.execute("""CREATE TABLE employees (
 #            first text,
 #            last text,
-#            integer id
+#            id integer
 #        )""")
 
 def insert_emp(emp):
     with conn:
         c.execute("INSERT INTO employees VALUES (:first, :last, :id)",{'first':emp.first,'last':emp.last,'id': emp.id})
 
-def get_emps_by_id(id):
-    c.execute("SELECT * FROM employees WHERE id=:id", {'id': id})
+def get_emps_by_name(lastname):
+    c.execute("SELECT * FROM employees WHERE last=:last", {'last': lastname})
     return c.fetchall()
 
 def remove_emp(emp):
@@ -27,7 +26,10 @@ def remove_emp(emp):
 emp_1 = Employee('Gal','Tovim',305484677)
 
 insert_emp(emp_1)
-emps=get_emps_by_id(emp_1)
+emps=get_emps_by_name('Tovim')
+print(emps)
+remove_emp(emp_1)
+emps=get_emps_by_name('Tovim')
 
 print(emps)
 
--- employee.py
+++ employee.py
@@ -1,15 +1,18 @@
 class Employee:
-        """Employee Class"""
-        def __init__(self,first,last,id):
-            self.first=first
-            self.last=last
-            self.id=id
+    """Employee Class"""
 
-        @property
-        def email(self):
-            return '{}.{}@email.com'.format(self.first,self.last)
-        @property
-        def fullname(selfself):
-            return '{} {}'.format(self.first,self.last)
-        def __repr__(self):
-            return "Employee('{}','{}',{})".format(self.first,self.last,last.id)
+    def __init__(self, first, last, id):
+        self.first = first
+        self.last = last
+        self.id = id
+
+    @property
+    def email(self):
+        return '{}.{}@email.com'.format(self.first, self.last)
+
+    @property
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+
+    def __repr__(self):
+        return "Employee {} {} {}".format(self.first, self.last, self.id)

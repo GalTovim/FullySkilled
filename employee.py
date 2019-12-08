33d0c95af9e4ab6e8a0d1d7b4fda65d58b5eb1b4
Gal Tovim <gal2077@gmail.com>
2019-12-08 15:42:44 +0200

    New Updated employee class 3:42 PM

--- employee.py
+++ employee.py
@@ -1,10 +1,14 @@
 class Employee:
     """Employee Class"""
 
-    def __init__(self, first, last, id, ):
+    def __init__(self, first, last, id, mail, cv, phone, skill):
         self.first = first
         self.last = last
         self.id = id
+        self.mail = mail
+        self.cv = cv
+        self.phone = phone
+        self.skill = skill
 
     @property
     def email(self):
@@ -15,4 +19,4 @@ class Employee:
         return '{} {}'.format(self.first, self.last)
 
     def __repr__(self):
-        return "Employee {} {} {}".format(self.first, self.last, self.id)
+        return "Employee {} {} {}".format(self.first, self.last, self.id, self.mail, self.cv, self.phone, self.skill)

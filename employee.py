41c96a05f38db75e8b7016a7af3eb87fa46f0636
Gal Tovim <gal2077@gmail.com>
2019-12-08 12:40:48 +0200

    Updated 12.8.19 12:40

---
+++ employee.py
@@ -0,0 +1,15 @@
+class Employee:
+        """Employee Class"""
+        def __init__(self,first,last,id):
+            self.first=first
+            self.last=last
+            self.id=id
+
+        @property
+        def email(self):
+            return '{}.{}@email.com'.format(self.first,self.last)
+        @property
+        def fullname(selfself):
+            return '{} {}'.format(self.first,self.last)
+        def __repr__(self):
+            return "Employee('{}','{}',{})".format(self.first,self.last,last.id)

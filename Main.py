8d22e1ff1ab60aa2eebfef58847dc97823cdf64d
Gal Tovim <gal2077@gmail.com>
2019-12-08 13:42:34 +0200

    New Main file

--- Main.py
+++ Main.py
@@ -18,19 +18,31 @@ def get_emps_by_name(lastname):
     c.execute("SELECT * FROM employees WHERE last=:last", {'last': lastname})
     return c.fetchall()
 
+def get_emps():
+    c.execute("SELECT * FROM employees")
+    return c.fetchall()
+
 def remove_emp(emp):
     with conn:
-        c.execute("DELETE from employees WHERE id=:id",
-                  {'id':emp.id})
+        c.execute(f"DELETE from employees WHERE id={emp}")
 
-emp_1 = Employee('Gal','Tovim',305484677)
+print("Please Choose What to do:")
+print("1. Add new Employee.")
+print("2. Update Employee.")
+print("3. Delete Employee.")
+choise=int(input("Enter your choise: "))
 
-insert_emp(emp_1)
-emps=get_emps_by_name('Tovim')
-print(emps)
-remove_emp(emp_1)
-emps=get_emps_by_name('Tovim')
-
-print(emps)
+if (choise==1):
+        Fname = input("Please Enter Employee Name: ")
+        Lname = input("Please Enter Employee Last Name: ")
+        ID = input("Please Enter Employee ID: ")
+        NewEmployee = Employee(Fname,Lname,ID)
+        insert_emp(NewEmployee)
+#Fname = input("Please Enter Employee Name: ")
+#Lname = input("Please Enter Employee Last Name: ")
+#ID = input("Please Enter Employee ID: ")
+#emp_1 = Employee(Fname,Lname,ID)
+#remove_emp(30548467)
+print(get_emps())
 
 conn.close()

fa89db05505e5145c63f7ad7bdc988d20828d73b
Tar Wolfson <tarwolfson@Tars-MacBook-Pro.local>
2019-12-12 12:20:53 +0200

    restarting

--- backend/db/connection.js
+++
@@ -1,7 +0,0 @@
-const monk = require("monk");
-
-const connectionString =
-  "mongodb+srv://tar:FullySkilled@cluster0-byfy8.mongodb.net/test?retryWrites=true&w=majority";
-const db = monk(connectionString);
-
-module.exports = db;

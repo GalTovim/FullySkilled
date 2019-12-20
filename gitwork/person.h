3d3bb871eeded9bc621b4656ac7767dcc546d4b9
YehonatamBinyamini <yonile2106@gmail.com>
2019-12-20 14:48:42 +0200

    person.h

---
+++ gitwork/person.h
@@ -0,0 +1,21 @@
+#ifndef PERSON_H
+#define PERSON_H
+
+
+#include <iostream>
+#include <string.h>
+#include <string>
+using namespace std;
+
+
+class Person {
+protected:
+	string Name; //10 chars maximum
+	long ID;
+	int Age;
+public:
+	Person(const Person& p1);
+	Person(string Name, long ID,int Age);
+	~Person();
+};
+#endif // !PERSON_H

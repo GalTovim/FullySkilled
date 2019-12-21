04dd8df07826f80e2656cf871dd34fb126904cd8
Gal Tovim <gal2077@gmail.com>
2019-12-21 21:59:12 +0200

    edited main file

--- gitwork/Main.cpp
+++ gitwork/Main.cpp
@@ -4,19 +4,28 @@
 #include "employee.h"
 
 int Main(){
-    int sizeOfarray;
-    char choice;
-    printf("Enter number of persons: ");
-    scanf("%d", &sizeOfarray);
-    Person *arr = new Person[sizeOfarray];
-    for(int i=0 ; i<sizeOfarray ; i++){
-        printf("Enter [E]mployee or [S]tudent: ");
-        scanf("%c", &choice);
-        if ((choice == 'E')||(choice == 'e')){
+    int sizeOfarray,choice;
+    Person** arr;
+
+    //student cin parameters
+    string nameStudent, instituteStudent;
+    int avgPerson,agePerson;
+    long idPerson;
+
+    cout<<"Enter number of Persons: ";
+    cin >> sizeOfarray;
+    arr = new Person*[sizeOfarray];
+    
+    for (int i=0 ; i<sizeOfarray ; i++){
+        printf("Enter [1[Employee or [2]Student: ");
+        scanf("%d", &choice);
+        if (choice==1){
+            cout << "Enter Student Name: ";
+            cin >> nameStudent;
+            cout << "Enter Student Institute: ";
+            cin >> instituteStudent;
             
-        }
-        if ((choice == 'S')||(choice == 's')){
-            
+            arr[i]= new Student();
         }
     }
     return 0;

0e5f2481d713e7204ebd47d37f3c2daf204e2dbf
yehuda <yv12345@gmail.com>
2019-12-22 21:24:14 +0200

    working student

---
+++ gitwork/workingStudent.cpp
@@ -0,0 +1,14 @@
+#include "workingStudent.h"
+#include ""
+
+workingStudent::workingStudent(workingStudent ws) : Employee
+{
+}
+
+workingStudent::workingStudent(int Average, string Institute, int Age, string name, long id, int salary)
+	:Student (Average, Institute, age, name , id)
+	Employee
+
+workingStudent ::~workingStudent()
+{
+}
--- gitwork/workingStudent.h
+++ gitwork/workingStudent.h
@@ -1,1 +1,14 @@
+#include"student.h"
 #pragma once
+using namespace std;
+
+class workingStudent : virtual public Student virtual public Employee
+{
+public:
+	workingStudent(workingStudent ws);
+	workingStudent();
+	~workingStudent();
+
+private:
+	bool same_institute;
+};

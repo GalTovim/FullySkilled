4cd46095accc144130e21883ce3ba279d6a484a3
YehonatamBinyamini <yonile2106@gmail.com>
2020-01-08 23:27:34 +0200

    add "addbusiness" methed in Eployers.vue

--- frontend/src/views/Employer.vue
+++ frontend/src/views/Employer.vue
@@ -15,10 +15,38 @@ export default {
         bussinessaddress: "",
         office: "", //anaf misra
         typejob: "",
+        salary: ""
       }
     };
   },
-  methods() {}
+  methods: {
+    addbussiness() {
+        const path = "http://localhost:5000/api/Employer";
+
+        if (this.input.bussinessname && this.input.bussinessaddress && this.input.office && this.input.typejob && this.input.salary )  {
+            const formData = new FormData();
+            formData.append("bussinessname", this.input.bussinessname);
+            formData.append("bussinessaddress", this.input.bussinessaddress);
+            formData.append("office", this.input.office);
+            formData.append("typejob", this.input.typejob);
+            formData.append("salary", this.input.salary);
+
+            axios
+            .post(path, formData, {
+                headers: {
+                "Content-Type": "multipart/form-data"
+                }
+            })
+            .then(res => {
+                if (res.data.error) {
+                this.alert.alertContent = res.data.error;
+                this.alert.showDismissibleAlert = true;
+                }
+            });
+        }
+        },
+      
+  }
 };
 </script>
 

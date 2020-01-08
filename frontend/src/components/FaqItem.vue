26ddabac81da007893a5b47f1bdf958718b1453f
tarwolfson <plasmaticshadows@gmail.com>
2020-01-08 20:03:08 +0200

    added faqitem

---
+++ frontend/src/components/FaqItem.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <b-card v-bind:title="question">{{answer}}</b-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "faq-item",
+  props: ["question", "answer"]
+};
+</script>
+
+<style>
+</style>

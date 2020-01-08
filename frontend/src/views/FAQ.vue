cbe885e62dc9ef4b009404d0973ced3b5f36478b
Gal Tovim <gal2077@gmail.com>
2020-01-08 20:19:43 +0200

    html template added

---
+++ frontend/src/views/FAQ.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <ul class="questions-list">
+      <li v-for="faq in faqs" v-bind:key="faq.id">
+        <Faqitem question="faq.quetion" answer="faq.answer" />
+      </li>
+    </ul>
+  </div>
+</template>

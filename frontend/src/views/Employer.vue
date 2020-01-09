6632d3dda02ae867207fd35ce6fbb38364cff8a0
YehonatamBinyamini <yonile2106@gmail.com>
2020-01-09 23:51:57 +0200

    update Employers.vue

--- frontend/src/views/Employer.vue
+++ frontend/src/views/Employer.vue
@@ -1,10 +1,18 @@
 <template>
-  <div>
+  <div id="Employer">
     <h1>Welcome {{user.username}}</h1>
+    <b-form-input type="text" name="bussinessname" v-model="input.bussinessname" placeholder="bussinessname" />
+    <b-form-input type="text" name="address" v-model="input.address" placeholder="address" />
+    <b-form-input type="text" name="office" v-model="input.office" placeholder="office" />
+    <b-form-input type="text" name="typejob" v-model="input.typejob" placeholder="typejob" />
+    <b-form-input type="text" name="salary" v-model="input.salary" placeholder="salary" />
+    <b-button type="button" @click="add_business()">add_business</b-button>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "employer",
   props: ["user"],
@@ -12,7 +20,7 @@ export default {
     return {
       input: {
         bussinessname: "",
-        bussinessaddress: "",
+        address: "",
         office: "", //anaf misra
         typejob: "",
         salary: ""
@@ -20,13 +28,13 @@ export default {
     };
   },
   methods: {
-    addbussiness() {
+    add_business() {
         const path = "http://localhost:5000/api/addBussiness";
 
         if (this.input.bussinessname && this.input.bussinessaddress && this.input.office && this.input.typejob && this.input.salary )  {
             const formData = new FormData();
             formData.append("bussinessname", this.input.bussinessname);
-            formData.append("bussinessaddress", this.input.bussinessaddress);
+            formData.append("address", this.input.address);
             formData.append("office", this.input.office);
             formData.append("typejob", this.input.typejob);
             formData.append("salary", this.input.salary);

0a2a565e7cd737fcdc9bd67f71ed0d73727687e1
YehonatamBinyamini <yonile2106@gmail.com>
2020-01-12 13:32:26 +0200

    added BusinessItem

---
+++ frontend/src/components/BusinessItem.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <b-card :title="name">
+      <b-list-group>
+        <b-list-group-item>Address: {{apartment}} {{street}}, {{city}}</b-list-group-item>
+        <b-list-group-item>Jobs:</b-list-group-item>
+        <b-list-group-item v-for="job in jobs" :key="job.id">{{job.title}}: {{job.description}}</b-list-group-item>
+      </b-list-group>
+      <b-button @click="addJob">Add Job</b-button>
+    </b-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BusinessItem",
+  props: ["name", "city", "street", "apartment", "jobs"],
+  data() {
+    return {};
+  },
+  methods: { addJob() {} }
+};
+</script>
+
+<style>
+</style>

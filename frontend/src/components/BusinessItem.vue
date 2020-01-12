2dfb68d51903fe2e2447f9f3a13c7c66b4eb7ca5
tarwolfson <plasmaticshadows@gmail.com>
2020-01-12 15:13:17 +0200

    added jobs in business

--- frontend/src/components/BusinessItem.vue
+++ frontend/src/components/BusinessItem.vue
@@ -4,7 +4,12 @@
       <b-list-group>
         <b-list-group-item>Address: {{apartment}} {{street}}, {{city}}</b-list-group-item>
         <b-list-group-item>Jobs:</b-list-group-item>
-        <b-list-group-item v-for="job in jobs" :key="job.id">{{job.title}}: {{job.description}}</b-list-group-item>
+        <b-list-group-item v-for="job in jobs" :key="job.id">
+          {{job.title}}: {{job.description}}
+          <ul>
+            <li v-for="item in job.applicants" :key="item.id">{{item}}</li>
+          </ul>
+        </b-list-group-item>
       </b-list-group>
       <b-button @click="addJob">Add Job</b-button>
     </b-card>

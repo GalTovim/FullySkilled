5cb11c18ba2e737584d080205eed2fd6b7ee8fb9
YehonatamBinyamini <yonile2106@gmail.com>
2020-01-12 13:32:43 +0200

    updated employer

--- frontend/src/views/Employer.vue
+++ frontend/src/views/Employer.vue
@@ -1,12 +1,65 @@
 <template>
   <div id="Employer">
+    <Navbar role="employer" />
     <h1>Welcome {{user.username}}</h1>
-    <b-form-input type="text" name="bussinessname" v-model="input.bussinessname" placeholder="bussinessname" />
-    <b-form-input type="text" name="address" v-model="input.address" placeholder="address" />
-    <b-form-input type="text" name="office" v-model="input.office" placeholder="office" />
-    <b-form-input type="text" name="typejob" v-model="input.typejob" placeholder="typejob" />
-    <b-form-input type="text" name="salary" v-model="input.salary" placeholder="salary" />
-    <b-button type="button" @click="add_business()">add_business</b-button>
+    <BusinessItem
+      v-for="business in businesses"
+      :key="business.id"
+      :name="business.name"
+      :city="business.city"
+      :street="business.street"
+      :apartment="business.apartment"
+      :jobs="business.jobs"
+    />
+
+    <b-modal
+      id="modal-prevent-closing"
+      ref="modal"
+      title="Enter business details"
+      @show="resetModal"
+      @hidden="resetModal"
+      @ok="handleOk"
+    >
+      <form ref="form" @submit.stop.prevent="handleSubmit">
+        <b-form-group
+          :state="nameState"
+          label="Name"
+          label-for="name-input"
+          invalid-feedback="Name is required"
+        >
+          <b-form-input id="name-input" v-model="name" :state="nameState" required></b-form-input>
+        </b-form-group>
+
+        <b-form-group
+          :state="cityState"
+          label="City"
+          label-for="city-input"
+          invalid-feedback="City is required"
+        >
+          <b-form-input id="city-input" v-model="city" :state="cityState" required></b-form-input>
+        </b-form-group>
+
+        <b-form-group
+          :state="streetState"
+          label="Street"
+          label-for="street-input"
+          invalid-feedback="Street is required"
+        >
+          <b-form-input id="street-input" v-model="street" :state="streetState" required></b-form-input>
+        </b-form-group>
+
+        <b-form-group
+          :state="apartmentState"
+          label="Apartment"
+          label-for="apartment-input"
+          invalid-feedback="Apartment is required"
+        >
+          <b-form-input id="apartment-input" v-model="apartment" :state="apartmentState" required></b-form-input>
+        </b-form-group>
+      </form>
+    </b-modal>
+
+    <b-button v-b-modal.modal-prevent-closing>Add Business</b-button>
   </div>
 </template>
 
@@ -15,50 +68,89 @@ import axios from "axios";
 import Navbar from "../components/Nav";
 import { mapState } from "vuex";
 
+import BusinessItem from "../components/BusinessItem";
+
 export default {
   name: "employer",
-  components: { Navbar },
+  components: { Navbar, BusinessItem },
   computed: {
     ...mapState(["user"])
   },
   data() {
     return {
-      input: {
-        bussinessname: "",
-        address: "",
-        office: "", //anaf misra
-        typejob: "",
-        salary: ""
-      }
+      businesses: [],
+      nameState: null,
+      cityState: null,
+      streetState: null,
+      apartmentState: null,
+      name: "",
+      city: "",
+      apartment: "",
+      street: ""
     };
   },
   methods: {
-    add_business() {
-        const path = "http://localhost:5000/api/addBussiness";
+    getBusinesses() {
+      const path = "http://localhost:5000/api/getBusinesses/";
 
-        if (this.input.bussinessname && this.input.bussinessaddress && this.input.office && this.input.typejob && this.input.salary )  {
-            const formData = new FormData();
-            formData.append("bussinessname", this.input.bussinessname);
-            formData.append("address", this.input.address);
-            formData.append("office", this.input.office);
-            formData.append("typejob", this.input.typejob);
-            formData.append("salary", this.input.salary);
+      axios.get(path + this.user.username).then(res => {
+        console.log(res.data.businesses);
+        this.businesses.push(...res.data.businesses);
+      });
+    },
+    resetModal() {
+      this.name = "";
+      this.city = "";
+      this.street = "";
+      this.apartment = "";
+      this.nameState = null;
+      this.cityState = null;
+      this.streetState = null;
+      this.apartmentState = null;
+    },
+    checkFormValidity() {
+      const valid = this.$refs.form.checkValidity();
+      return valid;
+    },
+    handleOk(bvModalEvt) {
+      // Prevent modal from closing
+      bvModalEvt.preventDefault();
+      // Trigger submit handler
+      this.handleSubmit();
+    },
+    handleSubmit() {
+      // Exit when the form isn't valid
+      if (!this.checkFormValidity()) {
+        return;
+      }
 
-            axios
-            .post(path, formData, {
-                headers: {
-                "Content-Type": "multipart/form-data"
-                }
-            })
-            .then(res => {
-                if (res.data.error) {
-                this.alert.alertContent = res.data.error;
-                this.alert.showDismissibleAlert = true;
-                }
-            });
-        }
-        },
-      
+      const path = "http://localhost:5000/api/addBusiness";
+
+      const formData = new FormData();
+      formData.append("businessname", this.name);
+      formData.append("city", this.city);
+      formData.append("street", this.street);
+      formData.append("apartment", this.apartment);
+      formData.append("owner", this.user.username);
+
+      axios
+        .post(path, formData, {
+          headers: {
+            "Content-Type": "multipart/form-data"
+          }
+        })
+        .then(res => console.log(res));
+      // Hide the modal manually
+      this.$nextTick(() => {
+        this.$refs.modal.hide();
+      });
+      this.getBusinesses();
+      // this.$router.go();
+    }
+  },
+
+  beforeMount() {
+    this.getBusinesses();
   }
 };
 </script>
